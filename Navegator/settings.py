3739e7bf59202f6acfbc32f09e771efbf8e847a4
DenSerKo <89607400164denis@gmail.com>
2024-11-17 19:40:39 +0300

    rq deploy

--- Navegator/settings.py
+++ Navegator/settings.py
@@ -1,3 +1,5 @@
+import os
+
 import dj_database_url
 """
 Django settings for Navegator project.
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core.apps.CoreConfig"
 ]
 
 MIDDLEWARE = [
@@ -131,7 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
